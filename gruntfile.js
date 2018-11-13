67f4cebf32649a9c76a27fbef40d02805667a793
wuwww990507 <1351169948@qq.com>
2018-11-13 16:35:23 +0800

    配置文件

--- gruntfile.js
+++ gruntfile.js
@@ -31,12 +31,25 @@ module.exports = function(grunt){
 		// 	}
 		// },
 		clean:{
-			dest:['dist/*']
+			dest:['dist/*','sample/js/*']
 		},
 		jshint:{
 			test:['src/js/wipe.js'],
 			options:{
 				jshintrc:'.jshintrc'
+			}
+		},
+		copy:{
+			js:{expand:true,cwd:"dist/js/",src:"*.js",dest:"sample/js/"}
+		},
+		replace:{
+			example:{
+				src:['sample/js/index.html'],
+				overwrite:true,
+				replacements:[{
+					from:/wipe-.\d[\.]\d[\.]\d\.min/g,
+					to:'wipe-<%=pkg.version%>.min.js'
+				}]
 			}
 		}
 	});
@@ -45,6 +58,8 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-text-replace');
 	// 告诉grunt当我们输入grunt命令需要做些什么，有先后顺序
-	grunt.registerTask('default',['jshint','clean','uglify',]);
+	grunt.registerTask('default',['jshint','clean','uglify','copy','replace']);
 }
